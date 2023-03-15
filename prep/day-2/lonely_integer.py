6dc1808f3d58790af2ccdfe32a9289583cb2f8a7
zd <zheladev@gmail.com>
2023-03-15 20:43:07 +0100

    Add lonely integer hackerrank link

--- prep/day-2/lonely_integer.py
+++ prep/day-2/lonely_integer.py
@@ -1,3 +1,4 @@
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two
 #!/bin/python3
 
 import math
